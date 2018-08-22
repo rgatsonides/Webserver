500201603d571a64d00ea85fb2f8c0c4a5e1615b
Rosanne Gatsonides <rgatsonides@sogyo.nl>
2018-08-22 13:25:06 +0200

    Connection handler met toegevoegde functies van request en response

--- src/main/java/nl/sogyo/webserver/ConnectionHandler.java
+++ src/main/java/nl/sogyo/webserver/ConnectionHandler.java
@@ -3,9 +3,16 @@ package nl.sogyo.webserver;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class ConnectionHandler implements Runnable {
+
     private Socket socket;
+    String ParameterValue;
+    String ParameterName;
+    String HeaderParameterName;
+    String HeaderParameterValue;
+    RequestMessage request;
 
     public ConnectionHandler(Socket toHandle) {
         this.socket = toHandle;
@@ -15,14 +22,35 @@ public class ConnectionHandler implements Runnable {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String line = reader.readLine();
+            request = new RequestMessage(line);
             System.out.println();
 
             while (!line.isEmpty()) {
                 System.out.println(line);
                 line = reader.readLine();
+                if (!line.isEmpty()) {
+                    request.NewLine(line);
+                }
             }
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            writer.write("Thank you for connecting!\r\n");
+
+            ResponseMessage responsemessage = new ResponseMessage(request);
+            responsemessage.setStatus(HttpStatusCode.OK);
+
+            String holeResponseMessage = "\n" + "You did an HTTP " + request.getHTTPMethod()
+                    + " request and you requested the following resource:"
+                    + request.getResourcePath() + "\n"
+                    + "The following header parameters where passed: " + "\n"
+                    + HeaderParameters() + "\n" + "\n"
+                    + "The following parameters where passed: " + "\n"
+                    + Parameters();
+
+            responsemessage.setContent(holeResponseMessage);
+            String[] words = holeResponseMessage.split("\n");
+            for (String word : words) {
+                writer.write(word);
+                writer.newLine();
+            }
             writer.flush();
             socket.close();
         } catch (IOException e) {
@@ -30,10 +58,32 @@ public class ConnectionHandler implements Runnable {
         }
     }
 
+    public String HeaderParameters() {
+        String Headerparameters = "";
+        for (int i = 0; i < request.getHeaderParameterNames().size(); i++) {
+            HeaderParameterName = request.getHeaderParameterNames().get(i);
+            HeaderParameterValue = request.getHeaderParameterValue(request.getHeaderParameterNames().get(i));
+            Headerparameters += HeaderParameterName + ":" + HeaderParameterValue + "\n";
+        }
+        return Headerparameters;
+    }
+
+    public String Parameters() {
+        String Parameters = "";
+        if (request.getParameterNames() != null) {
+            for (int i = 0; i < request.getParameterNames().size(); i++) {
+                ParameterName = request.getParameterNames().get(i);
+                ParameterValue = request.getParameterValue(request.getParameterNames().get(i));
+                Parameters += ParameterName + ":" + ParameterValue + "\n";
+            }
+        }
+        return Parameters;
+    }
+
     public static void main(String... args) {
         try {
             ServerSocket socket = new ServerSocket(9090);
-            while(true) {
+            while (true) {
                 Socket newConnection = socket.accept();
                 Thread t = new Thread(new ConnectionHandler(newConnection));
                 t.start();
